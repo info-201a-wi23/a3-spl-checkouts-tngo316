681c4e6e1df8ba643f892445b0b3175c6a7bb1ea
tngo316 <tngo2@uw.edu>
2023-02-24 01:49:34 -0800

    nice work buddy

--- chart1_SPL.R
+++ chart1_SPL.R
@@ -3,12 +3,12 @@ library(dplyr)
 library(plotly)
 my_dataSet <-read.csv("~/Desktop/2022-2023-All-Checkouts-SPL-Data.csv", stringsAsFactors = FALSE)
 
-data_for_2022 <- my_dataSet %>% filter(CheckoutYear == "2022")
+agg_data2 <- my_dataSet %>%
+  filter(MaterialType %in% c("SOUNDDISC", "VIDEODISC")) %>%
+  group_by(CheckoutYear, CheckoutMonth, MaterialType) %>%
+  summarize(TotalCheckouts = sum(Checkouts))
 
-chart1 <- ggplot(data_for_2022) + geom_col(aes(x = CheckoutMonth, y = Checkouts, fill = UsageClass)) + 
-  scale_color_brewer(palette = "Set1") +
-  labs(title = "Ebooks Checkouts vs. Books Checkouts", x = "Months", y = "Number of Checkouts") + 
+chart1 <- ggplot(agg_data2, aes(x = CheckoutMonth, y = TotalCheckouts, color = MaterialType)) +
+  geom_line() +
+  labs(x = "Month", y = "Total Checkouts", title = "Trend of Sound discs and Video discs over Time") +
   scale_x_continuous(breaks = seq(1, 12))
-
-ggplotly(chart1)
-
